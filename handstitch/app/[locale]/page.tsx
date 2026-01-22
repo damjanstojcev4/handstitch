7aede44827af4ff8a0e63047500f8e089759e4a4
Damjan Stojchev <damjan.stojcev4@gmail.com>
2026-01-22 14:53:16 +0100

    gallery view

--- handstitch/app/[locale]/page.tsx
+++ handstitch/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "../components/hero/HeroSection";
 import WalletConfigurator from "../components/canvas/WalletConfigurator";
 import AboutSection from "../components/about/AboutSection";
 import { useTranslations } from "next-intl";
+import ProductMasonryGallery from "../components/gallery/ProductMasonryGallery";
 
 export default function Home() {
     const t = useTranslations("configurator");
@@ -10,8 +11,9 @@ export default function Home() {
         <main className="w-full">
             <HeroSection />
             <AboutSection />
+            <ProductMasonryGallery />
 
-            <section className="py-20 px-6">
+            <section id="builder" className="py-20 px-6">
                 <div className="max-w-7xl mx-auto">
                     <h1 className="text-3xl font-bold mb-6">
                         {t("title")}
